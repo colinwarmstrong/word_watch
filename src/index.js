3e02324306b91d718dca46dd819043229d820d6c
Colin Armstrong <colinwarmstrong@gmail.com>
2018-10-10 11:23:55 -0600

    Completes second user story, user can post new words to the database.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import $ from 'jquery'
 let baseUrl = 'https://wordwatch-api.herokuapp.com'
 
 $(document).ready(() => {
+  getFavoriteWord()
+})
+
+const getFavoriteWord = () => {
+  $('.word-count').html('')
   fetch(`${baseUrl}/api/v1/top_word`)
     .then(result => result.json())
     .then(favoriteWord => appendFavoriteWord(favoriteWord))
     .catch(error => ({ error }))
-})
+}
 
 const appendFavoriteWord = (favoriteWord) => {
   let word = Object.keys(favoriteWord['word']).pop();
@@ -16,6 +21,33 @@ const appendFavoriteWord = (favoriteWord) => {
     <p>${word}: ${quantity}</p>`)
 }
 
+const addWords = () => {
+  let userText = $('textarea#text-area').val();
+  let textArray = userText.split(' ');
+  textArray.forEach(function(word) {
+    addWord(word);
+  });
+  getFavoriteWord();
+};
+
 const addWord = (word) => {
-  
+  fetch(`${baseUrl}/api/v1/words`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      word: {
+        value: `${word}`
+      }
+    })
+  })
 }
+
+const wordLoop = (word) => {
+  let i = 0;
+  while (i < 400) {
+    addWord(word);
+    i++;
+  }
+}
+
+$('#break-down-button').click(addWords);
